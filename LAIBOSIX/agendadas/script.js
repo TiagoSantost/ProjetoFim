8080893f7b5c997a8ef8b59661a5ecfb16c0baa7
TiagoSantost <tiagofranciscot1@gmail.com>
2022-06-07 23:27:20 -0300

    atualizando

--- LAIBOSIX/agendadas/script.js
+++ LAIBOSIX/agendadas/script.js
@@ -4,28 +4,8 @@ var tarefa = JSON.parse(jsonTarefa);
 
 
 
+console.log(tarefa[0].data.nome)
 
 
-
-
-render()
-
-
-function render(){
-    const tbody = document.getElementById('listaRegistroBody')
-    if(tbody){
-        var data = tarefa;
-        console.log(data)
-        data.map( medico => {
-                return `<tr>
-                        <td>${medico.nome}</td>
-                        <td>${medico.especialidade}</td>
-                        <td>${medico.horario}</td>
-                        <td>
-                            
-                        </td>
-                    </tr>`
-            } )
-        tbody.innerHTML = data.join('')
-    }
-}
+let teste = document.getElementById('nome')
+teste.innerHTML = tarefa[0].data.nome;
